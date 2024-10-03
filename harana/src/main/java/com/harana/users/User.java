45f821a0275e7b55df037604e8ec5b9ac1ee2afe
Raindeeru <derv.gonzales17@gmail.com>
2024-10-04 01:17:37 +0800

    login

--- harana/src/main/java/com/harana/users/User.java
+++ harana/src/main/java/com/harana/users/User.java
@@ -3,7 +3,9 @@ package com.harana.users;
 import java.util.ArrayList;
 
 public class User {
-    private boolean online = false;
+    private boolean online;
     private ArrayList<Chat> chats;
     private ArrayList<Post> posts;
+    private ArrayList<String> imagePaths;
+    private ArrayList<String> musicUrls;
 }
