1794267a90d02ab366429a3c4efd2766b4396366
Rin1803 <karrinfrida@gmail.com>
2024-10-04 00:53:24 +0800

    change name lol

--- harana/src/main/java/com/harana/users/Post.java
+++ harana/src/main/java/com/harana/users/Post.java
@@ -1,5 +1,12 @@
 package com.harana.users;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class Post {
-    
+    @FXML TextArea postArea; 
+    @FXML Button backButton; 
+    @FXML Button postButton; 
+
 }
