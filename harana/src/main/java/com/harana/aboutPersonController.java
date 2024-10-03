97f76a1a9a147beb62bdfc1bdf956c4f7e94948a
Rin1803 <karrinfrida@gmail.com>
2024-10-04 04:25:46 +0800

    ewan ko na

---
+++ harana/src/main/java/com/harana/aboutPersonController.java
@@ -0,0 +1,34 @@
+package com.harana;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class aboutPersonController {
+    @FXML Button backButton; 
+    @FXML Button likeButton; 
+    @FXML Button dislikeButton; 
+    @FXML Button nextImage; 
+    @FXML Button backImage; 
+
+    @FXML
+    public void backButton() { 
+        
+    }
+
+    @FXML
+    public void likeButton() { 
+        
+    }
+
+    @FXML
+    public void dislikeButton() { 
+        
+    }
+
+    @FXML
+    public void backImage() { 
+        
+    }
+
+}
