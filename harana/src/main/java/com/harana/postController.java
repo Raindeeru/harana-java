2c1899b756448123f170847b7fb4a0c36e81396d
Rin1803 <karrinfrida@gmail.com>
2024-10-04 03:08:04 +0800

    idk what i did pls

--- harana/src/main/java/com/harana/postController.java
+++ harana/src/main/java/com/harana/postController.java
@@ -1,13 +1,30 @@
 package com;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;;
 
 public class postController {
     @FXML TextArea postArea; 
     @FXML Button backButton; 
     @FXML Button postButton; 
+
+    Stage stage; 
+
+    @FXML
+    public void backB() throws IOException{ 
+        stage = (Stage) backButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    public void postB() throws IOException{ 
+        System.out.println("Hello");
+    }
 
     
 }
