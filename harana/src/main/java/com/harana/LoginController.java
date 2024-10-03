cc95cc2eb4a6479ebe5f7c7c8f6913fcce8784fc
Ghastly <Ghaztlyy@gmail.com>
2024-10-04 01:51:06 +0800

    I added the GUI fxml

---
+++ harana/src/main/java/com/harana/LoginController.java
@@ -0,0 +1,20 @@
+package com.harana;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    void loginBtn(ActionEvent event) {
+
+    }
+
+}
