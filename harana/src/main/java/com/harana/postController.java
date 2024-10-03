70b9ecafbae1f363be778363e0b296c1748977c6
Rin1803 <karrinfrida@gmail.com>
2024-10-04 01:06:16 +0800

    May iniba lang po

---
+++ harana/src/main/java/com/harana/postController.java
@@ -0,0 +1,13 @@
+package com;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Button;
+
+public class postController {
+    @FXML TextArea postArea; 
+    @FXML Button backButton; 
+    @FXML Button postButton; 
+
+    
+}
--- harana/src/main/java/com/harana/users/Post.java
+++ harana/src/main/java/com/harana/users/Post.java
@@ -1,12 +1,5 @@
 package com.harana.users;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-
 public class Post {
-    @FXML TextArea postArea; 
-    @FXML Button backButton; 
-    @FXML Button postButton; 
 
 }
