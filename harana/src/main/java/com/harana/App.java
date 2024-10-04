fa3f39b07218dc82fbfebe0aff3195697f693e70
Ghastly <Ghaztlyy@gmail.com>
2024-10-04 16:54:48 +0800

    Appnyava

--- harana/src/main/java/com/harana/App.java
+++ harana/src/main/java/com/harana/App.java
@@ -20,7 +20,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("loginGUI"), 250, 480);
+        scene = new Scene(loadFXML("loginGUI"));
         stage.setScene(scene);
         stage.show();
     }
