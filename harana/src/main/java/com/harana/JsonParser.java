a6056c8e1eea0f1a359356d0c450e4594f13332f
Raindeeru <derv.gonzales17@gmail.com>
2024-10-04 16:33:42 +0800

    json shit

--- harana/src/main/java/com/harana/JsonParser.java
+++ harana/src/main/java/com/harana/JsonParser.java
@@ -1,8 +1,11 @@
 package com.harana;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.harana.users.Cred;
 import com.harana.users.User;
 import java.io.*;
+import java.util.ArrayList;
 
 public class JsonParser {
     private static String users_loc = "data/users/";
@@ -21,5 +24,15 @@ public class JsonParser {
     
     public static void setUser(String userURL, User user){
 
+    }
+
+    public ArrayList<Cred> getCredentials(String credentialsURL){
+        Gson gson = new Gson();
+        BufferedReader bufferedReader = new BufferedReader(
+            new FileReader(cred_loc + credentialsURL)
+        );
+        Type listType = new TypeToken<ArrayList<App>>(){}.getType();
+        ArrayList<Cred> credentials = credentials.fromJson(bufferedReader, listType);
+        return credentials;
     }
 }
