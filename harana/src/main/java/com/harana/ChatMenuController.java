4cc58b703993bcf4c4faae98f4c327533bbb36d2
Raindeeru <derv.gonzales17@gmail.com>
2024-10-05 00:05:55 +0800

    aaaa

---
+++ harana/src/main/java/com/harana/ChatMenuController.java
@@ -0,0 +1,5 @@
+package com.harana;
+
+public class ChatMenuController {
+    
+}
--- harana/src/main/java/com/harana/ChatsController.java
+++ harana/src/main/java/com/harana/ChatsController.java
@@ -39,6 +39,9 @@ public class ChatsController {
     @FXML
     Label typing_label;
 
+    @FXML
+    ScrollPane chat_scroll;
+
     Chat chat;
 
     User user;
@@ -48,17 +51,21 @@ public class ChatsController {
     Task<Void> checkChats;
     Thread chatChecker;
 
+    VBox intermediarryBox;
+
     boolean typing = false;
+    int oldi = 0;
 
     @FXML
     public void initialize() throws IOException{
         checkChats = new Task<Void>() {
             @Override
             protected Void call() throws Exception {
-                while (true) {
+                while (true) { 
                     Thread.sleep(100);
                     Chat checkChat = JsonParser.getChat(chat.getChatid() + ".json");
                     if (isUser1){
+                        System.out.println("hahah");
                         if (checkChat.isUser2_typing()) {
                             typing_label.setVisible(true);
                         }else{
@@ -79,7 +86,7 @@ public class ChatsController {
         chatChecker.setDaemon(true);
         chatChecker.start();
         chat = JsonParser.getChat("chat1.json");
-        user = JsonParser.getUser("user2.json");
+        user = JsonParser.getUser("user1.json");
         if (user.getUsername().equals(chat.getUser1())) {
             isUser1 = true;
         }else{
@@ -88,22 +95,7 @@ public class ChatsController {
         chat = JsonParser.getChat("chat1.json");
         chat_screen.setStyle("-fx-border-color:blue");
         for(Message message: chat.getMessages()){
-            String name;
-            if (message.getSender().equals("user1") && isUser1) {
-                name = "You";
-            }else if(message.getSender().equals("user1") && !isUser1){
-                name = chat.getUser2();
-            }else if (message.getSender().equals("user2") && !isUser1) {
-                name = "You";
-            }else {
-                name = chat.getUser2();
-            }
-            Label userName = new Label(name);
-            Label messageLabel = new Label(message.getMessage());
-            VBox userChat = new VBox();
-            userChat.setStyle("-fx-border-color:red");
-            userChat.getChildren().addAll(userName, messageLabel);
-            chat_screen.getChildren().addAll(userChat);
+            NewChat(message);
         }
         chat_screen.getChildren().remove(user1_chat);
         chat_screen.getChildren().remove(user1_message);
@@ -116,9 +108,17 @@ public class ChatsController {
     
 
     @FXML
-    private void Send(){
-        System.out.println(message_field.getText());
-        System.out.println(chat);
+    private void Send() throws IOException{
+        Message message;
+        if (isUser1) {
+            message = new Message("user1", message_field.getText());
+        }
+        else{
+            message = new Message("user2", message_field.getText());
+        }
+        NewChat(message);
+        chat.getMessages().add(message);
+        JsonParser.setChat(chat.getChatid() + ".json", chat);
     }
 
     @FXML 
@@ -167,5 +167,28 @@ public class ChatsController {
             user = JsonParser.getUser("user1.json");
             isUser1 = true;
         }
+    }
+
+    private void NewChat(Message message){
+        String name;
+        if (message.getSender().equals("user1") && isUser1) {
+            name = "You";
+            System.out.println("hhahaha");
+        }else if(message.getSender().equals("user1") && !isUser1){
+            name = chat.getUser2();
+            System.out.println("iiiii");
+        }else if (message.getSender().equals("user2") && !isUser1) {
+            name = "You";
+            System.out.println("kakkakakaka");
+        }else {
+            name = chat.getUser2();
+            System.out.println("ooooooooo");
+        }
+        Label userName = new Label(name);
+        Label messageLabel = new Label(message.getMessage());
+        VBox userChat = new VBox();
+        userChat.setStyle("-fx-border-color:red");
+        userChat.getChildren().addAll(userName, messageLabel);
+        chat_screen.getChildren().add(userChat);
     }
 }
