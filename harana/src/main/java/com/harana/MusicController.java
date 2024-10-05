3b999b02006c9d198a2f13ee7ab57d5e249681a1
Raindeeru <derv.gonzales17@gmail.com>
2024-10-05 13:53:51 +0800

    may music na

--- harana/src/main/java/com/harana/MusicController.java
+++ harana/src/main/java/com/harana/MusicController.java
@@ -11,10 +11,6 @@ public class MusicController {
 
     @FXML
     private void initialize() throws IOException, InterruptedException{
-        HttpClient client = HttpClient.newHttpClient();
-
-        HttpRequest req = HttpRequest.newBuilder().uri(URI.create("https://youtube-to-mp315.p.rapidapi.com/download?url=https://www.youtube.com/watch?v=9s-U4I5uOIg&t=289s&format=mp3")).POST(HttpRequest.BodyPublishers.noBody()).header("x-rapidapi-key", "3aaca02681msh9a97c8cc81c1f95p1099d3jsne44a150eaee0").header("x-rapidapi-host", "youtube-to-mp315.p.rapidapi.com").header("Content-Type", "application/json").build();
-        HttpResponse<String> res = client.send(req, BodyHandlers.ofString());
-        System.out.println(res.body());
+        MusicManager.getMusicFile("https://www.youtube.com/watch?v=MZA7W6n6bBA", "\test.mp3");
     }
 }
--- harana/src/main/java/com/harana/MusicManager.java
+++ harana/src/main/java/com/harana/MusicManager.java
@@ -2,16 +2,26 @@ package com.harana;
 
 import java.net.http.*;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.io.IOException;
 import java.net.URI;
+import java.net.URL;
+import java.io.*;
+import java.nio.channels.*;;
 
 public class MusicManager {
     
-    public static void getMusicFile(String URL) throws IOException, InterruptedException{
+    public static void getMusicFile(String URL, String FilePath) throws IOException, InterruptedException{
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest req = HttpRequest.newBuilder().uri(URI.create("https://youtube-to-mp315.p.rapidapi.com/download?url=" + URL + "&format=mp3")).POST(HttpRequest.BodyPublishers.noBody()).header("x-rapidapi-key", "3aaca02681msh9a97c8cc81c1f95p1099d3jsne44a150eaee0").header("x-rapidapi-host", "youtube-to-mp315.p.rapidapi.com").header("Content-Type", "application/json").build();
         HttpResponse<String> res = client.send(req, BodyHandlers.ofString());
+        MusicResponse musicRes = JsonParser.getMusicResponse(res.body());
         System.out.println(res.body());
+        System.out.println(musicRes.getDownloadUrl());
+        
+        ReadableByteChannel readableByteChannel = Channels.newChannel(URI.create(musicRes.getDownloadUrl()).toURL().openStream());
+        FileOutputStream fileOutputStream = new FileOutputStream(FilePath);
+        FileChannel fileChannel = fileOutputStream.getChannel();
+        
+        fileChannel.transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+        fileOutputStream.close();
     }
-
 }
