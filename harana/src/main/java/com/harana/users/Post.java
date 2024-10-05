9943b03ec81b30a9103bc2cc3d143e585cb00d8e
Rin1803 <karrinfrida@gmail.com>
2024-10-06 03:49:56 +0800

    POST DONE I HATE JSON!!!!!!

--- harana/src/main/java/com/harana/users/Post.java
+++ harana/src/main/java/com/harana/users/Post.java
@@ -3,5 +3,17 @@ package com.harana.users;
 import java.util.Date;
 
 public class Post {
-    String postContent;
+    String userPostC;
+
+    public Post(String userPostC) {
+        this.userPostC = userPostC;
+    }
+
+    public String getPostContent() {
+        return userPostC;
+    }
+    
+    public void setPostContent(String userPostC) {
+        this.userPostC = userPostC;
+    }
 }
